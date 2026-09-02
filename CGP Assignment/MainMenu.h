962833c9a4fd0394202d0f644dfaa10916640fed
KahzehisHere <kahzeh01@gmail.com>
2026-09-02 21:10:28 +0800

    Parallax Menu
    Moving Background

--- CGP Assignment/MainMenu.h
+++ CGP Assignment/MainMenu.h
@@ -6,25 +6,47 @@
 #include <vector>
 #include "Level1.h"
 #include "Level2.h"
+#include "ParallaxLayer.h"
 #include <Windows.h>
 
-class MainMenu :
-    public Game
+class MainMenu :    public Game
 {
+private:
+
+    ParallaxLayer NebulaBG;
+    ParallaxLayer FarStarsBG;
+    ParallaxLayer MiddleStarsBG;
+    ParallaxLayer PlanetBG;
+    ParallaxLayer DenseStarsBG;
+    ParallaxLayer NearStarsBG;
+
+    ParallaxLayer AsteroidBG1;
+    ParallaxLayer AsteroidBG2;
+
 public:
-    GameObject* level1Button;
-    GameObject* level2Button;
-    GameObject* quitGameButton;
-    GameObject* muteButton;
-    GameObject* minusButton;
-    GameObject* addButton;
-    GameObject* unmuteButton;
-    GameObject* mouseCursor;
+    GameObject* gameTitle = nullptr;
+    GameObject* level1Button = nullptr;
+    GameObject* level2Button = nullptr;
+    GameObject* quitGameButton = nullptr;
+
+    GameObject* muteButton = nullptr;
+    GameObject* minusButton = nullptr;
+    GameObject* addButton = nullptr;
+    GameObject* unmuteButton = nullptr;
+
+    GameObject* mouseCursor = nullptr;
+
     int volume = 100;
-    void InitialiseGame();
-    void Update();
-	void Render();
-    void CleanUp();
+
     MainMenu();
+
+    void InitialiseGame() override;
+
+    void Update() override;
+
+	void Render() override;
+
+    void CleanUp() override;
+    
 };
 
---
+++ CGP Assignment/ParallaxLayer.cpp
@@ -0,0 +1,91 @@
+#include "ParallaxLayer.h"
+#include <iostream>
+
+using namespace std;
+
+ParallaxLayer::ParallaxLayer()
+{
+    texture = NULL;
+
+    speed = 0.0f;
+    segmentWidth = 0.0f;
+    yPosition = 0.0f;
+
+    ZeroMemory(&sourceRect, sizeof(sourceRect));
+
+    for (int i = 0; i < 3; i++)
+    {
+        position[i] = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+    }
+}
+
+ParallaxLayer::~ParallaxLayer()
+{
+}
+
+
+bool ParallaxLayer::Initialise(const char* textureName,RECT rect,float moveSpeed,float y)
+{
+    HRESULT hr =
+        D3DXCreateTextureFromFileA(DirectXManager::myVirtualGPU,textureName,&texture);
+
+    if (FAILED(hr))
+    {
+        cout << "Failed to load: " << textureName << endl;
+        return false;
+    }
+
+    sourceRect = rect;
+
+    speed = moveSpeed;
+    yPosition = y;
+
+    segmentWidth = static_cast<float>(sourceRect.right - sourceRect.left);
+
+    position[0] =D3DXVECTOR3(0.0f,yPosition,0.0f);
+
+    position[1] =D3DXVECTOR3(segmentWidth,yPosition,0.0f);
+
+    position[2] =D3DXVECTOR3(segmentWidth * 2.0f,yPosition,0.0f);
+
+    return true;
+}
+
+
+void ParallaxLayer::Update()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        position[i].x -= speed;
+
+        if (position[i].x <= -segmentWidth)
+        {
+            position[i].x +=
+                segmentWidth * 3.0f;
+        }
+    }
+}
+
+void ParallaxLayer::Render()
+{
+    if (!texture)
+        return;
+
+    D3DXMATRIX identityMatrix;
+    D3DXMatrixIdentity(&identityMatrix);
+
+    DirectXManager::spriteBrush->SetTransform(&identityMatrix);
+    for (int i = 0; i < 3; i++)
+    {
+        DirectXManager::spriteBrush->Draw(texture,&sourceRect,NULL,&position[i],D3DCOLOR_XRGB(255,255,255));
+    }
+}
+
+void ParallaxLayer::CleanUp()
+{
+    if (texture)
+    {
+        texture->Release();
+        texture = NULL;
+    }
+}
